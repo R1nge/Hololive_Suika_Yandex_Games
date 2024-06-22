2013b59b9e5bee2c3ff3d1598b7a63a9fc62eb10
R1nge <r1ngegamedev@gmail.com>
2024-06-22 15:25:57 +0300

    Loading UI

---
+++ Assets/_Assets/Scripts/Services/UIs/LoadingIconRotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace _Assets.Scripts.Services.UIs
+{
+    public class LoadingIconRotator : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+        [SerializeField] private GameObject loadingIcon;
+
+        private void LateUpdate() => loadingIcon.transform.Rotate(new Vector3(0, 0, 90) * (speed * Time.deltaTime));
+    }
+}
