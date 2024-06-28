9836c17d866e35109f44ce66bf0cb6b7ab29f654
R1nge <r1ngegamedev@gmail.com>
2024-06-28 20:31:11 +0300

    New sprites

---
+++ Assets/_Assets/Scripts/Services/UIs/ComboUIView.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+using VContainer;
+
+namespace _Assets.Scripts.Services.UIs
+{
+    public class ComboUIView : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI comboText;
+        [Inject] private ComboService _comboService;
+
+        private void Start()
+        {
+            _comboService.OnComboChanged += SetCombo;
+        }
+
+        private void SetCombo(int combo) => comboText.text = combo.ToString();
+
+        private void OnDestroy() => _comboService.OnComboChanged -= SetCombo;
+    }
+}
