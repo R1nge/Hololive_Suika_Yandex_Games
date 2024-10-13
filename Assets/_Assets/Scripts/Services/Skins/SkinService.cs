025a27a41556f48f41a5cf40cc5f136ceddb73f7
R1nge <r1ngegamedev@gmail.com>
2024-10-13 15:16:39 +0300

    feat: fubuki locked sprite

---
+++ Assets/_Assets/Scripts/Services/Skins/SkinService.cs
@@ -0,0 +1,21 @@
+﻿using _Assets.Scripts.Configs;
+using UnityEngine;
+
+namespace _Assets.Scripts.Services.Skins
+{
+    public class SkinService
+    {
+        private readonly ConfigProvider _configProvider;
+
+        private SkinService(ConfigProvider configProvider)
+        {
+            _configProvider = configProvider;
+        }
+    }
+
+    public struct SuikaSkinData
+    {
+        public SuikaConfig.SuikaSkin SuikaSkin;
+        public bool IsLocked;
+    }
+}
