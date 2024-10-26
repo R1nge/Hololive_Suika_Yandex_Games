d8d65c308a14501161cadd0b535cf66a1d1e3dd8
R1nge <r1ngegamedev@gmail.com>
2024-10-26 17:42:45 +0300

    feat: imported furniture sprites pack

---
+++ Assets/_Assets/Scripts/Misc/ResetProgress.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using YG;
+
+namespace _Assets.Scripts.Misc
+{
+    public class ResetProgress : MonoBehaviour
+    {
+        public void ResetProgressYG()
+        {
+            YandexGame.ResetSaveProgress();
+            YandexGame.SaveProgress();
+        }
+    }
+}
