ed154ede3f399c372af028ef5abdfc3f605256f8
R1nge <r1ngegamedev@gmail.com>
2024-10-27 22:35:47 +0300

    feat: go to the interactable, before the interaction

--- Assets/_Assets/Scripts/Houses/Interactables/InteractableController.cs
+++ Assets/_Assets/Scripts/Houses/Interactables/InteractableController.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using Pathfinding;
 using UnityEngine;
 
 namespace _Assets.Scripts.Houses.Interactables
 {
     public class InteractableController : MonoBehaviour
     {
+        [SerializeField] private AIPath character;
+        private IInteractable _target;
+        private Transform _targetTransform;
+
         private void Update()
         {
             if (Input.GetMouseButtonDown(0))
@@ -15,9 +20,33 @@ namespace _Assets.Scripts.Houses.Interactables
                 {
                     if (hit.transform.TryGetComponent(out IInteractable interactable))
                     {
-                        interactable.Interact();
+                        _target = interactable;
+                        if (hit.transform.TryGetComponent(out GridCellView cell))
+                        {
+                            _targetTransform = cell.transform;
+                            if (Vector2.Distance(character.position, cell.transform.position) > 1f)
+                            {
+                                return;
+                            }
+
+                            _target.Interact();
+                        }
                     }
                 }
+            }
+
+            if (!float.IsPositiveInfinity(character.remainingDistance) && character.remainingDistance < 1f)
+            {
+                if (_target == null || _targetTransform == null) return;
+
+                if (Vector2.Distance(character.position, _targetTransform.position) > 1f)
+                {
+                    return;
+                }
+
+                _target.Interact();
+                _target = null;
+                _targetTransform = null;
             }
         }
     }
