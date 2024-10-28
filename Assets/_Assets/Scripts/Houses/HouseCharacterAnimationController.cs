afea9d8b6a9e881fae42d2a70b1919a084a5d508
R1nge <r1ngegamedev@gmail.com>
2024-10-28 19:52:04 +0300

    feat: snejok animation controller

---
+++ Assets/_Assets/Scripts/Houses/HouseCharacterAnimationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Pathfinding;
+using UnityEngine;
+
+namespace _Assets.Scripts.Houses
+{
+    public class HouseCharacterAnimationController : MonoBehaviour
+    {
+        [SerializeField] private AIPath aiPath;
+        [SerializeField] private Animator animator;
+        private static readonly int Running = Animator.StringToHash("Running");
+
+        private void Update()
+        {
+           UpdateAnimation();
+        }
+
+        private void UpdateAnimation()
+        {
+            if (aiPath.reachedDestination)
+            {
+                animator.SetBool(Running, false);
+            }
+            else
+            {
+                animator.SetBool(Running, true);
+            }
+        }
+    }
+}
