cf1a34e5106ba89d99348e327706ae23dbe08b1d
R1nge <r1ngegamedev@gmail.com>
2024-10-28 20:03:08 +0300

    feat: house character rotate towards click; fix snejok running animation

---
+++ Assets/_Assets/Scripts/Houses/HouseCharacterRotationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Pathfinding;
+using UnityEngine;
+
+namespace _Assets.Scripts.Houses
+{
+    public class HouseCharacterRotationController : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+                Vector3 spritePosition = transform.position;
+
+                if (mousePosition.x < spritePosition.x)
+                {
+                    spriteRenderer.flipX = true;
+                }
+                else
+                {
+                    spriteRenderer.flipX = false;
+                }
+            }
+        }
+    }
+}
